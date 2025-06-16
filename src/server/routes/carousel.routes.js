77bfdbf768ae0d8ee6714449e4deb7dc113c4c42
JulioMaravilla <juliomaravilla47@gmail.com>
2025-06-16 09:48:29 -0600

    si

--- src/server/routes/carousel.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCarouselImages } = require('../controllers/carousel.controller');
-
-// Ruta para obtener las imágenes del carrusel
-router.get('/images', getCarouselImages);
-
-module.exports = router;
